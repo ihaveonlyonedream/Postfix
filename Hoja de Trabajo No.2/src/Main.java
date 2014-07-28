2bdf08460c9c9acd63785b29f9887cf1ba057114
Luis <Luis@192.168.1.8>
2014-07-28 00:15:22 -0600

    Final

--- Hoja de Trabajo No.2/src/Main.java
+++ Hoja de Trabajo No.2/src/Main.java
@@ -1,8 +1,12 @@
+//******************************************************************************************************************
 //Universidad del Valle de Guatemala
-//Erick Saucedo 13167
-//Kimberly Guzman 13642
-//Luis Gomez 13135
-
+//Autores:                  Erick Saucedo       13167
+//                          Kimberly Guzmanm    13642
+//                          Luis Gomez          13135
+//
+//Descripcion: Este programa lee un documento de texto llamado Datos.txt donde se encuentra una operacion en
+//             postfix, luego de leer el documento realiza la operacion en infix y despliega el resultado.
+//******************************************************************************************************************
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -11,17 +15,24 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//******************************************************************************************************************
 
 public class Main {
 
     public static void main(String[] args) {
        
+        //******************************************************************************************************************
+        
+        //Variables a utilizar
         Pila operandos = new Pila();
         int resultado = 0;
         int operando1 = 0;
         int operando2 = 0;
         
+        
+        //******************************************************************************************************************
+        
+        //Lectura del documento Datos.txt
         try{
             //Se abre el archivo Datos.txt y se lee linea por linea. 
             FileInputStream archivo = new FileInputStream("C:\\Users\\Luis\\Desktop\\Java\\postfix\\Postfix\\Hoja de Trabajo No.2\\src\\Datos.txt");
@@ -29,78 +40,115 @@ public class Main {
             BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
             String linea;
             
+        //******************************************************************************************************************    
+            
+            //Se lee linea por linea
             while ((linea = buffer.readLine()) != null)   {
+                
+                //Se reinician las variables de las operaciones por si existe mas de una linea de operaciones
+                resultado=0;
+                operando1=0;
+                operando2=0;
                 
                 //Se eliminan los espacios del string y se agregan a un arreglo de strings.
                 String[] lineaArray = linea.split(" ");
+        //******************************************************************************************************************
                 
+                //Se recorre el array con los numeros utilizados
                 for (int i = 0; i < lineaArray.length; i++) {
                     
+                    //Se verifica si el caracter es un numero o un operador
                     try{
-                        
+                        //Si es un numero se guarda en la pila
                         operandos.push(Integer.parseInt(lineaArray[i]));
                         
-                    }catch(Exception e){
+                    }catch(Exception e){                                        //Si no se puede convertir en número se verifica que tipo de operador es
+        //******************************************************************************************************************
                         
+                        //SUMA
                         if(lineaArray[i].equals("+")){
                             
                             operando1 = (int) operandos.pop();
+                            //Verifica si en la pila existe otro numero para operar, de lo contrario tomara el resultado como segundo operando.                            
                             if ((operandos.peek())!= null){
                                 operando2 = (int)operandos.pop();
                             }else{
                                 operando2= resultado;
                             }
+                            
+                            //Realiza la operacion
                             resultado = operando2 + operando1;
-                            System.out.println( operando2 + " + " + operando1);
-                            System.out.println (resultado);
+                            
                         }
+        //******************************************************************************************************************                
+                        //RESTA
                         
                         if(lineaArray[i].equals("-")){
+                            
                              operando1 = (int) operandos.pop();
+                            
+                             //Verifica si en la pila existe otro numero para operar, de lo contrario tomara el resultado como segundo operando.                            
                             if ((operandos.peek())!= null){
                                 operando2 = (int)operandos.pop();
                             }else{
                                 operando2= resultado;
                             }
+                            
+                            //Realiza la operacion
                             resultado = operando2 - operando1;
-                            System.out.println( operando2 + " - " + operando1);
-                            System.out.println (resultado);
+                            
                         }
+        //******************************************************************************************************************                
+                        //MULTIPLICACION
                         
                         if(lineaArray[i].equals("*")){
                         
                              operando1 = (int) operandos.pop();
+                             
+                            //Verifica si en la pila existe otro numero para operar, de lo contrario tomara el resultado como segundo operando.                             
                             if ((operandos.peek())!= null){
                                 operando2 = (int)operandos.pop();
                             }else{
                                 operando2= resultado;
                             }
+                            
+                            //Realiza la operacion
                             resultado = operando2 * operando1;
-                            System.out.println( operando2 + " * " + operando1);
-                            System.out.println (resultado);
+                            
                         }
+        //******************************************************************************************************************                
+                        //DIVISION
                         
                         if(lineaArray[i].equals("/")){
                         
                             operando1 = (int) operandos.pop();
+                            
+                            //Verifica si en la pila existe otro numero para operar, de lo contrario tomara el resultado como segundo operando.                            
                             if ((operandos.peek())!= null){
                                 operando2 = (int)operandos.pop();
                             }else{
                                 operando2= resultado;
                             }
+                            
+                            //Realiza la operacion
                             resultado = operando2 / operando1;
-                            System.out.println( operando2 + " / " + operando1);
-                            System.out.println (resultado);
+                            
                         }
                         
-                        
+        //******************************************************************************************************************                
                     }
                     
                     
                     
                 }
+        //******************************************************************************************************************        
+                //Muestra el resultado
+                System.out.println("El resultado es: " + resultado);
+        //******************************************************************************************************************
             }
             entrada.close();
+            
+            
         }catch (Exception e){ 
             System.err.println("Archivo no encontrado");
         }
--- Hoja de Trabajo No.2/src/Pila.java
+++ Hoja de Trabajo No.2/src/Pila.java
@@ -1,20 +1,30 @@
+//******************************************************************************************************************
 //Universidad del Valle de Guatemala
-//Erick Saucedo 13167
-//Kimberly Guzmanm 13642
-//Luis Gomez 13135
-
+//Autores:                  Erick Saucedo       13167
+//                          Kimberly Guzmanm    13642
+//                          Luis Gomez          13135
+//
+//Descripcion: Este programa es utilizado para generar una pila de datos donde se puede ingresar un valor,
+//             sacar el ultimo de valor de pila y eliminarlo, mostrar el ultimo valor de la pila y saber si
+//             la pila esta vacia.
+//******************************************************************************************************************
 
 
 import java.util.ArrayList;
 
 public class Pila {
     
+    //Se crea un arrayList de tipo objeto
     private ArrayList <Object> pila = new ArrayList();
     
+    
+    //******************************************************************************************************************
+    //Ingreso de un valor a la pila
     public void push(Object o){
        pila.add(o);
     }
-
+    
+    //Egreso de un valor de la pila y luego lo elimina
     public Object pop(){
        if(!(pila.isEmpty())){
            Object o = pila.get(pila.size()-1);
@@ -24,7 +34,8 @@ public class Pila {
            return null;
        }
     }
-
+    
+   //Muestra el ultimo valor ingresado a la pila
    public Object peek(){
           if(!(pila.isEmpty())){
            Object o = pila.get(pila.size()-1);
@@ -33,9 +44,12 @@ public class Pila {
            return null;
        }    
    }
-
+   
+   //Verifica si la pila esta vacia o no
    public boolean empty(){
        return pila.isEmpty();
     }
-
+   
+   //******************************************************************************************************************
+   
 }
