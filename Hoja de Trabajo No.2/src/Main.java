de380c53434bddd5cf0bfc8d0b22a2973a2f7364
Luis <Luis@192.168.1.8>
2014-07-27 22:29:18 -0600

    Primera prueba de commit

---
+++ Hoja de Trabajo No.2/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        try{
+            FileInputStream fstream = new FileInputStream("LeerArchivo.java");
+            DataInputStream entrada = new DataInputStream(fstream);
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+            String strLinea;
+            
+            while ((strLinea = buffer.readLine()) != null)   {
+                 System.out.println (strLinea);
+            }
+            entrada.close();
+        }catch (Exception e){ 
+            System.err.println("Archivo no encontrado");
+        }*/
+       
+    }
+}
---
+++ Hoja de Trabajo No.2/src/Pila.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+
+public class Pila {
+    
+    private ArrayList <Object> pila = new ArrayList();
+    
+    public void push(Object o){
+       pila.add(o);
+    }
+
+    public Object pop(){
+       if(!(pila.isEmpty())){
+           Object o = pila.get(pila.size()-1);
+           pila.remove(pila.size()-1);
+           return o;
+       }else{
+           return null;
+       }
+    }
+
+   public Object peek(){
+          if(!(pila.isEmpty())){
+           Object o = pila.get(pila.size()-1);
+           return o;
+       }else{
+           return null;
+       }    
+   }
+
+   public boolean empty(){
+       return pila.isEmpty();
+    }
+
+}
