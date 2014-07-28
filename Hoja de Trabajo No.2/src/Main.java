e1b1252cc8dfb5b992588e4f45dcec1e91feaac1
Luis <Luis@192.168.1.8>
2014-07-27 23:13:37 -0600

    Separando operandos y mostrandolos en infix

--- Hoja de Trabajo No.2/src/Main.java
+++ Hoja de Trabajo No.2/src/Main.java
@@ -17,8 +17,10 @@ public class Main {
 
     public static void main(String[] args) {
        
+        Pila operandos = new Pila();
+        
         try{
-            
+            //Se abre el archivo Datos.txt y se lee linea por linea. 
             FileInputStream archivo = new FileInputStream("C:\\Users\\Luis\\Desktop\\Java\\postfix\\Postfix\\Hoja de Trabajo No.2\\src\\Datos.txt");
             DataInputStream entrada = new DataInputStream(archivo);
             BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
@@ -26,10 +28,28 @@ public class Main {
             
             while ((linea = buffer.readLine()) != null)   {
                 
+                //Se eliminan los espacios del string y se agregan a un arreglo de strings.
                 String[] lineaArray = linea.split(" ");
                 
                 for (int i = 0; i < lineaArray.length; i++) {
-                    System.out.println(lineaArray[i]);
+                    
+                    try{
+                        
+                        operandos.push(Integer.parseInt(lineaArray[i]));
+                        
+                    }catch(Exception e){
+                        
+                        while(operandos.empty()==false){
+                            System.out.println(operandos.pop());
+                            System.out.println(lineaArray[i]);
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
                 }
             }
             entrada.close();
